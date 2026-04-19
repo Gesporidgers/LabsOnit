ac9c508b65dea0f4099de55ab62025efe5bdf8c7
Gesporidgers <49988107+Gesporidgers@users.noreply.github.com>
2026-04-19 14:01:38 +0300

    лаба 3

--- FlightTODO/tests.py
+++ FlightTODO/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Flight
 
-# Create your tests here.
+class FlightModelTest(TestCase):
+    def test_flight_creation(self):
+        flight = Flight.objects.create(
+            origin="Moscow",
+            origin_icao="UUEE",
+            dest="Saint Petersburg",
+            dest_icao="ULLI",
+            completed=False
+        )
+        self.assertEqual(flight.origin, "Moscow")
+        self.assertEqual(flight.dest, "Saint Petersburg")
+        self.assertFalse(flight.completed)
